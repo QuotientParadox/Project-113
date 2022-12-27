4966bb724192ac6cbab8d0e25fc51acac3c7a392
QuotientParadox <117741113+QuotientParadox@users.noreply.github.com>
2022-12-26 20:39:15 -0500

    made the image have a frame

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
 function preload()
 {
-    img = loadImage("https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Birthday_candles.jpg/1200px-Birthday_candles.jpg")
+    //img = loadImage("https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Birthday_candles.jpg/1200px-Birthday_candles.jpg")
 }
 function setup()
 {
-    canvas = createCanvas(640, 480);
-    canvas.position(110, 250);
+    createCanvas(640, 480);
+    // canvas.position(110, 250);
+    // tint_color = "";
     video = createCapture(VIDEO);
-    video.hide();
-    tint_color = "";
+    video.hide()
 }
 function draw()
 {
-    image(video,0,0,640,480);
-    tint(tint_color);
-    image(img, 0, 0, 320, 240);
-
+    image(video, 0, 0, 640, 480);
+    fill(255,0,0);
+    circle(80, 80, 70);
+    circle(560, 80, 70);
+    circle(80, 400, 70);
+    circle(560, 400, 70);
+    fill(0, 255, 0);
+    rect(110, 65, 420, 20);
+    rect(70, 105, 20, 270);
+    rect(550, 105, 20, 270);
+    rect(110, 390, 420, 20);
 }
 function take_snapshot()
 {
-    save("max.png");
+    save("picture.png");
 }
 function filter_tint()
 {
